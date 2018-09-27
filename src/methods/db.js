c6f60d09aa067a6470da3eefe0c1eef16352a95f
Fégan PIERRE <fegan.pierre@gmail.com>
2018-09-27 12:02:20 +0200

    update db connection

--- src/methods/db.js
+++ src/methods/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql')
 
 const db_connection = mysql.createConnection({
   host: 'localhost',
-  database: 'test_strw',
-  user: 'nana',
-  password: ''
+  database: 'heroku_1c14da866ac803a',
+  user: 'b08feda83cf450',
+  password: 'cb911d7a'
 })
 
 module.exports = db_connection

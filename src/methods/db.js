bd8fbf72018e614a405531c9cef0a6c005d14b38
Fégan PIERRE <fegan.pierre@gmail.com>
2018-10-01 17:21:47 +0200

    Install "cross-env" and modify db.js login info
    Modified db.js to be able to specify which database to use when in dev or prod

--- src/methods/db.js
+++ src/methods/db.js
@@ -1,10 +1,23 @@
 const mysql = require('mysql')
 
-const db_connection = mysql.createConnection({
-  host: 'us-cdbr-iron-east-01.cleardb.net',
-  database: 'heroku_1c14da866ac803a',
-  user: 'b08feda83cf450',
-  password: 'cb911d7a'
-})
+const connectToDB = () => {
+  const currentEnv = process.env.NODE_ENV ? process.env.NODE_ENV : 'development'
+  if (currentEnv === 'development') {
+    console.log('dev environement: connected to db \'test_strw\' as \'nana\'')
+    return db_connection = mysql.createConnection({
+      host: 'localhost',
+      database: 'test_strw',
+      user: 'nana',
+      password: ''
+    })
+  }
+  return mysql.createConnection({
+    host: 'us-cdbr-iron-east-01.cleardb.net',
+    database: 'heroku_1c14da866ac803a',
+    user: 'b08feda83cf450',
+    password: 'cb911d7a'
+  })
 
-module.exports = db_connection
+}
+
+module.exports = connectToDB()

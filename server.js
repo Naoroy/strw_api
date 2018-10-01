fe18465f4d965e10da2ed1ce5fd91acc1727367f
Fégan PIERRE <fegan.pierre@gmail.com>
2018-10-01 17:36:02 +0200

    Allow CORS when in development

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const routes = require('./router')
 const PORT = process.env.PORT || 5000
 
 express()
+  .use((req, res, next) => {
+    if (process.env.NODE_ENV === 'development') {
+      res.header('Access-Control-Allow-Origin', '*')
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    }
+    next()
+  })
   .use(express.json())
   .use(routes)
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => {
+    console.log(`Listening on ${ PORT }`)
+    if (process.env.NODE_ENV === 'development') {
+      console.log('CORS Allowed!')
+    }
+  })
